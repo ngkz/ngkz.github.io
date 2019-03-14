167908b55756a626f9511ac997e5ca09f226e360
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2019-03-14 19:37:56 +0900

    simplify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 #TODO: upgrade git and use git-worktree.
 set -e
+shopt -s extglob
 
-TEMPLATE_COMMIT=be94da0318fa2f742e1bf99826b565d51074a658
 BOLD_CYAN="\033[1m\033[36m"
 NORMAL="\033[0m"
 
-echo "${BOLD_CYAN}Cleaning public directory${NORMAL}"
+echo -e "${BOLD_CYAN}Cleaning public directory${NORMAL}"
 cd public
-git reset --hard HEAD
-git checkout master
-git read-tree "$TEMPLATE_COMMIT"
-git clean -fdx
+# Delete all files except .git
+rm -rf !(.git|.|..)
+# Bypassing jekyll
+touch .nojekyll
 cd ..
 
-echo "${BOLD_CYAN}Generating site${NORMAL}"
+echo -e "${BOLD_CYAN}Building site${NORMAL}"
 hugo
 
-echo "${BOLD_CYAN}Pushing site${NORMAL}"
+echo -e "${BOLD_CYAN}Pushing site${NORMAL}"
 cd public
 git add --all .
-git commit -m "generate site"
+git commit -m "rebuild site"
 git push origin master
