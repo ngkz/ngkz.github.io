18750f69d30c2bbd9d4f03f6012a50be6d7350cb
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-11-06 00:00:47 +0900

    move build artifacts to /build and add CACHEDIR.TAG

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-hugo
-tar -Jcvf public.tar.xz public
+mkdir -p build
+echo "Signature: 8a477f597d28d172789f06886806bc55" >build/CACHEDIR.TAG
+hugo -d build/public
+tar -Jcvf build/public.tar.xz -C build public
