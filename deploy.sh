33095f8c3597988e7a41cc615df3c4a7fa224a65
Kazutoshi Noguchi <kn@f2l.cc>
2024-12-09 08:51:29 +0900

    switched back to github pages. new git-worktree deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
 set -euo pipefail
+shopt -s extglob
 
-mkdir -p build
-echo "Signature: 8a477f597d28d172789f06886806bc55" >build/CACHEDIR.TAG
-rm -rf build/public
-hugo -d build/public
-tar -Jcvf build/public.tar.xz -C build public
+if tty >/dev/null; then
+    BOLD_CYAN="\033[1m\033[36m"
+    NORMAL="\033[0m"
+else
+    BOLD_CYAN=
+    NORMAL=
+fi
+
+echo -e "${BOLD_CYAN}Cleaning public directory${NORMAL}"
+git worktree add public master
+cd public
+# Delete all files except .git
+rm -rf !(.git|.|..|.nojekyll|CNAME)
+# Bypassing jekyll
+touch .nojekyll
+cd ..
+
+echo -e "${BOLD_CYAN}Building site${NORMAL}"
+hugo
+
+echo -e "${BOLD_CYAN}Pushing site${NORMAL}"
+cd public
+git add --all .
+if ! git status | grep "nothing to commit" >/dev/null; then
+    git commit -m "rebuild site"
+fi
+git push origin master
+
+cd ..
+git worktree remove public
